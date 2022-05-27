39e01daa6ca202b182d7cdab7f00e6ae26210367
Sergio Radigales <sergioradigales@gmail.com>
2022-05-27 17:02:04 +0200

    2.2: Course information step7

--- part-1/course information/src/App.js
+++ part-1/course information/src/App.js
@@ -19,13 +19,14 @@ const Part = (props) => {
 const Total = (props) => {
 
   const total = 
+    props.exercises + 
     props.exercises1 + 
-    props.exercises2 + 
-    props.exercises3 
+    props.exercises2 +
+    props.exercises3
 
   return (
     <footer>
-      <p>Number of exercicses {total}</p>
+      <p><b>Number of exercicses {total}</b></p>
     </footer>
   )
 }
@@ -44,6 +45,14 @@ const Course = (props) => {
       <Part 
       name = {props.name2}
       exercises = {props.exercises2}/>
+      <Part 
+      name = {props.name3}
+      exercises = {props.exercises3}/>
+      <Total
+      exercises = {props.exercisesSum}
+      exercises1 = {props.exercises1Sum}
+      exercises2 = {props.exercises2Sum}
+      exercises3 = {props.exercises3Sum}/>
    </div>
    
  )
@@ -68,6 +77,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: "Redux",
+        exercises: 11,
+        id: 4
       }
     ]
   }
@@ -81,6 +95,12 @@ const App = () => {
       exercises1 = {course.parts[1].exercises}
       name2 = {course.parts[2].name}
       exercises2 = {course.parts[2].exercises}
+      name3 = {course.parts[3].name}
+      exercises3 = {course.parts[3].exercises}
+      exercisesSum = {course.parts[0].exercises}
+      exercises1Sum = {course.parts[1].exercises}
+      exercises2Sum = {course.parts[2].exercises}
+      exercises3Sum = {course.parts[3].exercises}
       />
   )
 }

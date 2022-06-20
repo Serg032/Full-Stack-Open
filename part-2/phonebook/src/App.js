430c44823f2ec7373b9070826a66d598e182791e
Serg032 <sergioradigales@gmail.com>
2022-06-20 17:50:24 +0200

    2.15: Phonebook step7

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -1,94 +1,100 @@
-import "./App.css"
-import { useEffect, useState } from 'react'
-import Title from "./components/title"
-import Filter from "./components/filter"
-import AddContact from "./components/addContact"
-import Main from "./components/main"
-import axios from "axios"
+import "./App.css";
+import { useEffect, useState } from "react";
+import Title from "./components/title";
+import Filter from "./components/filter";
+import AddContact from "./components/addContact";
+import Main from "./components/main";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState("")
-  const [filtered, setFiltered] = useState("")
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filtered, setFiltered] = useState("");
 
   useEffect(() => {
-    axios
-    .get("http://localhost:3001/persons")
-    .then(response => {
-      setPersons(response.data)
-    })
-  }, [])
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+      console.log("reponse", response.data);
+    });
+  }, [persons.length]);
 
-  
   const handleChangeName = (e) => {
-    setNewName(e.target.value)
-  }
+    setNewName(e.target.value);
+  };
 
   const handleChangePhone = (e) => {
-    setNewNumber(e.target.value)
-  }
+    setNewNumber(e.target.value);
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const newPerson = {
-      id: persons[persons.length -1].id + 1,
       name: newName,
-      number: newNumber
-    }
-    if (persons.find(p => p.name === newPerson.name)){
-      alert(`${newPerson.name} name already exists`)
-      setNewName("")
-      setNewNumber("")
-    } 
-    else if (persons.find(p => p.phoneNumber === newPerson.number)){
-      alert(`${newPerson.phoneNumber} number already exists`)
-      setNewName("")
-      setNewNumber("")
+      number: newNumber,
+      id: persons[persons.length - 1].id + 1,
+    };
+    if (persons.find((p) => p.name === newPerson.name)) {
+      alert(`${newPerson.name} name already exists`);
+      setNewName("");
+    } else if (persons.find((p) => p.number === newPerson.number)) {
+      alert(`${newPerson.number} number already exists`);
+      setNewNumber("");
     } else {
-      setPersons(persons.concat(newPerson))
-      setNewName("")
-      setNewNumber("")
+      axios
+        .post(" http://localhost:3001/persons", newPerson)
+        .then((response) => {
+          console.log("added", response);
+          setPersons(persons.concat(response.data));
+          setNewName("");
+          setNewNumber("");
+        });
     }
-  }
+  };
 
   const handleChangeFilter = (event) => {
-    setFiltered(event.target.value)
-  }
+    setFiltered(event.target.value);
+  };
 
   const filtering = (str) => {
-    const result = persons.filter(person => person.name.toLowerCase().includes(str.toLowerCase()))
-    if(result === ([])){
-      return <p><b>No contacts found</b></p>
+    const result = persons.filter((contact) =>
+      contact.name.toLowerCase().includes(str.toLowerCase())
+    );
+    if (result === []) {
+      return (
+        <p>
+          <b>No contacts found</b>
+        </p>
+      );
     }
-    return result.map(contact => <p key={contact.name}>{contact.id}. {contact.name}. {contact.number}</p>)
-  }
+    return result.map((contact) => (
+      <p key={contact.name}>
+        {contact.id}. {contact.name}. {contact.number}
+      </p>
+    ));
+  };
 
-  const clear = () => setFiltered("")
+  const clear = () => setFiltered("");
 
   return (
     <div id="main">
-      <Title title = "Phonebook"/>
+      <Title title="Phonebook" />
       <Filter
-      value = {filtered}
-      eventHandler = {handleChangeFilter}
-      function = {clear}
+        value={filtered}
+        eventHandler={handleChangeFilter}
+        function={clear}
       />
-      <br/>
+      <br />
       <AddContact
-      eventSubmit = {handleSubmit}
-      valueName = {newName}
-      eventName = {handleChangeName}
-      valueNumber = {newNumber}
-      eventNumber = {handleChangePhone}
+        eventSubmit={handleSubmit}
+        valueName={newName}
+        eventName={handleChangeName}
+        valueNumber={newNumber}
+        eventNumber={handleChangePhone}
       />
-      <Main
-      value = {filtered}
-      arr = {persons}
-      function = {filtering(filtered)}
-      />
+      <Main value={filtered} arr={persons} function={filtering(filtered)} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

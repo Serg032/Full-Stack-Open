184edf3e4a59ded0aa3b7b37206083a2c92827cd
Serg032 <sergioradigales@gmail.com>
2022-06-23 22:44:55 +0200

    3.1: Phonebook backend step1

---
+++ part-3/phonebook backend/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+
+let data = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.use(express.json());
+
+app.get("*", (request, response) => {
+  response.json(data);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});

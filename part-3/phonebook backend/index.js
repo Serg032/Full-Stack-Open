4317000c6bb3dd1a4393203e08fc40f68788ceba
Serg032 <sergioradigales@gmail.com>
2022-06-26 18:07:20 +0200

    3.6: Phonebook backend step6

--- part-3/phonebook backend/index.js
+++ part-3/phonebook backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+const morgan = require(`morgan`);
 
 let data = [
   {
@@ -24,7 +25,15 @@ let data = [
     number: "39-23-6423122",
   },
 ];
+const process = (request, response, next) => {
+  console.log("Learning Express.js");
+  console.log("REMEMBER. Issue at 3.6, Post error handling");
+  next();
+};
+
+app.use(process);
 app.use(express.json());
+app.use(morgan(`tiny`));
 
 app.get("/api/persons", (request, response) => {
   response.json(data);
@@ -59,31 +68,31 @@ const generateId = () => {
 };
 
 app.post(`/api/persons/`, (request, response) => {
-  const requestBody = request.body;
-  const names = data.map((p) => p.name);
-  console.log("names", names[0]);
-  console.log("rb", requestBody.name);
+  const body = request.body;
   const person = {
     id: generateId(),
-    name: requestBody.name,
-    number: requestBody.number,
+    name: body.name,
+    number: body.number,
   };
-  if (!request.body.name) {
+  if (!body.name) {
     return response.status(400).json({ error: "name missing." });
-  } else if (!request.body.number) {
+  } else if (!body.number) {
     return response.status(400).json({ error: "name missing." });
-  } else if (requestBody.name === names.map((name) => name)) {
-    response.status(400).json({ error: `${requestBody.name} already exists.` });
+  } else if (data.filter((person) => person.name === body.name).length > 0) {
+    return response
+      .status(400)
+      .json({ error: `${person.name} already exists.` });
+  } else if (
+    data.filter((person) => person.number === body.number).length > 0
+  ) {
+    return response
+      .status(400)
+      .json({ error: `${person.number} already exists.` });
   } else {
     data = data.concat(person);
-    console.log(
-      "Data updated.",
-      data.map((p) => p)
-    );
-    return response.status(200).json({ message: `Person created` });
+    return response.status(200).json({ message: "Person created" });
   }
 });
-console.log(data.map((element) => element.name));
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   data = data.filter((p) => p.id !== id);
